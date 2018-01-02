dc81db7a7420674661a49c2b57e0e384ce9319c9
zhulin peng <zhulin.peng@hermes-microvision.com>
2018-01-02 11:47:18 -0800

    mongoose part 2

---
+++ db.js
@@ -0,0 +1,29 @@
+// var mongoose = require('mongoose');
+// var DB_URL = 'mongodb://localhost:27017/zhulin';
+
+// mongoose.connect(DB_URL);
+
+// mongoose.connection.on('connected',()=>{
+// 	console.log("mongoose connected");
+// });
+
+// mongoose.connection.on('error',()=>{
+// 	console.log("mongoose connection error");
+// });
+
+// mongoose.connection.on('disconnected', ()=>{
+// 	console.log('mongoose disconnected');
+// });
+
+
+// //thsi si for Jainshu
+// var mongoose = require('mongoose');
+//         mongoose.connect('mongodb://localhost/test');
+
+//         var Cat = mongoose.model('Cat', { name: String });
+
+//     var kitty = new Cat({ name: 'Zildjian' });
+//     kitty.save(function (err) {
+//         if (err) // ...
+//         console.log('meow');
+//     });
